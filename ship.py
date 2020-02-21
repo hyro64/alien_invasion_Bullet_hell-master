9801338d638051a16c81f9c0b62deaab51bdac8f
Christian Mandujano Borjas <cmandujano10121990@gmail.com>
2020-02-21 10:54:58 -0700

    Fixed issue of ship not loading. Resources not in correct folder.

--- ship.py
+++ ship.py
@@ -1,18 +1,20 @@
 import pygame
 
 class Ship():
-    """Initialize the ship and set it's starting position."""
+
     def __init__(self,screen):
+        """Initialize the ship and sit it's starting position."""
         self.screen = screen
 
         # Load th ship image and get its rect.
-        self.image = pygame.image.load(images/ship.bmp)
+        self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
 
         # Start each new ship at the bottom center of the screen.
         self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_bottom
+        self.rect.bottom = self.screen_rect.bottom
 
-def blitme(self):
-    """Draw the ship at its current location."""
-    self.screen.blit(self.image, self.rect)
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
