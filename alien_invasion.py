e5001be792ea4803e4df6b1faf56d6ad87e3828c
Hyro64 <cmandujano10121990@gmail.com>
2021-08-11 13:40:27 -0600

    Files are refactored to original state. Will need new images for alien.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,45 +1,36 @@
-import sys
-
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 
 def run_game():
-    # Initialize game and creates a screen object(old)
-    # Initialize pygame, settings, and screen_object.
+    # Initialize pygame, settings, and screen object.
     pygame.init()
-
     ai_settings = Settings()
-
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
-
     pygame.display.set_caption("Alien Invasion")
 
-    # Make a Ship
+    # Set the background color.
+    bg_color = (230, 230, 230)
+
+    # Make a ship.
     ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in.
+    bullets = Group()
+    # Make an alien.
+    alien = Alien(ai_settings, screen)
 
-    # Set the background color RGB.
-    bg_color = (0, 0, 115)
-
-    # Start the main loop for thself.rect.centerx = self.screen_rect.centerxe game.
+    # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
-        gf.update_screen(ai_settings, screen, ship)
-
-        # Watch for keyboard and mouse events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        # Redraw the Screen during each pass through the loop
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # Make the mos recently drawn screen visible.
-        pygame.display.flip()
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
